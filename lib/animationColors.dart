6e73cc151022386fa699ed1d124a5c9c99252a0f
Phnix01 <fomar9235@gmail.com>
2024-10-26 13:38:49 +0100

    utilisation des animations explicites avec les couleurs ont été réaliseée avec succès

--- lib/animationColors.dart
+++ lib/animationColors.dart
@@ -7,18 +7,60 @@ class Animationcolors extends StatefulWidget {
   State<Animationcolors> createState() => _AnimationcolorsState();
 }
 
-class _AnimationcolorsState extends State<Animationcolors> {
+class _AnimationcolorsState extends State<Animationcolors>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _animationController;
+  late Animation<Color?> _animation;
+  bool _isAlreadyChanged = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController =
+        AnimationController(vsync: this, duration: Duration(seconds: 3));
+    _animation =
+        ColorTween(begin: Colors.deepOrangeAccent, end: Colors.greenAccent)
+            .animate(_animationController);
+    _animationController.forward();
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
           child: Center(
         child: Column(
-          children: [],
+          children: [
+            AnimatedBuilder(
+                animation: _animation,
+                builder: (context, child) {
+                  return Container(
+                    width: 200,
+                    height: 200,
+                    color: _animation.value,
+                  );
+                })
+          ],
         ),
       )),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: () {
+          setState(() {
+            if (_isAlreadyChanged) {
+              _animationController.reverse();
+            } else {
+              _animationController.forward();
+            }
+            _isAlreadyChanged = !_isAlreadyChanged;
+          });
+        },
         child: Icon(Icons.chair),
       ),
     );
